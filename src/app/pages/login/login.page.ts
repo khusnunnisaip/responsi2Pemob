f50110aed11794016b45bf4b37ad43b1c11668a0
khusnunnisaip <102377626+khusnunnisaip@users.noreply.github.com>
2023-12-01 23:57:50 +0700

    Update login.page.ts

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,3 @@
-// src/app/pages/login/login.page.ts
 import { Component } from '@angular/core';
 import { NavController } from '@ionic/angular';
 
@@ -14,13 +13,10 @@ export class LoginPage {
   constructor(private navCtrl: NavController) {}
 
   login() {
-    // Implementasi logika autentikasi
-    // Contoh sederhana: Cek jika username dan password sesuai
+
     if (this.username === 'admin' && this.password === 'password') {
-      // Redirect ke halaman inventaris setelah login sukses
       this.navCtrl.navigateForward('/inventory');
     } else {
-      // Tampilkan pesan kesalahan jika login gagal
       console.error('Login gagal. Periksa username dan password.');
     }
   }
